753fe75f8c82e4ebdbc300b03ef4425895efa326
rajibdas-it <rajibrad@gmail.com>
2022-10-11 02:08:44 +0600

    nav logo remove

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -56,16 +56,6 @@ const Header = () => {
                 About us
               </a>
             </li>
-            <li>
-              <a
-                href="/"
-                className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
-                aria-label="Sign up"
-                title="Sign up"
-              >
-                Sign up
-              </a>
-            </li>
           </ul>
           <div className="lg:hidden">
             <button
@@ -100,24 +90,7 @@ const Header = () => {
                         title="Company"
                         className="inline-flex items-center"
                       >
-                        <svg
-                          className="w-8 text-deep-purple-accent-400"
-                          viewBox="0 0 24 24"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          strokeLinecap="round"
-                          strokeMiterlimit="10"
-                          stroke="currentColor"
-                          fill="none"
-                        >
-                          <rect x="3" y="1" width="7" height="12" />
-                          <rect x="3" y="17" width="7" height="6" />
-                          <rect x="14" y="1" width="7" height="6" />
-                          <rect x="14" y="11" width="7" height="12" />
-                        </svg>
-                        <span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
-                          Company
-                        </span>
+                        e<span className="text-blue-500">Tutor</span>
                       </a>
                     </div>
                     <div>
