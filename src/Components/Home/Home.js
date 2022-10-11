f0c300a6a165bd90cf03424f13f14544ece3e106
rajibdas-it <rajibrad@gmail.com>
2022-10-11 21:36:24 +0600

    home page retouch

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,7 +1,6 @@
-
 import React, { useContext } from "react";
 import Lottie from "lottie-react";
-import banner from '../../Assets/banner.json'
+import banner from "../../Assets/banner.json";
 import { QuizContext } from "../layout/Main";
 import Quizs from "../Quizs/Quizs";
 
@@ -11,11 +10,14 @@ const Home = () => {
     <div className="mt-6 w-[80%] mx-auto">
       {/* banner section */}
       <section>
-        <div className="bg-teal-100 w-[80%] mx-auto flex flex-col md:flex-row lg:flex-row justify-center items-center">
+        <div className="bg-teal-100 w-full mx-auto flex flex-col md:flex-row lg:flex-row justify-center items-center rounded-md">
           <div className="w-[40%] m-3">
-            <h1 className="text-3xl font-bold text-blue-500 italic"
-            >EDUCATION SOLUTIONS</h1>
-            <p className="text-gray-500 italic">Do different today and every day.</p>
+            <h1 className="text-3xl font-bold text-blue-500 italic text-center">
+              EDUCATION SOLUTIONS
+            </h1>
+            <p className="text-gray-500 italic ml-8 text-center">
+              Do different today and every day.
+            </p>
           </div>
           <div className="w-[60%] m-3 sm:text-center">
             <Lottie animationData={banner} loop={true} />
@@ -26,12 +28,11 @@ const Home = () => {
       {/* cart section */}
       <section>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2">
-          {
-            quizs.map(quiz => <Quizs key={quiz.id} quiz={quiz}></Quizs>)
-          }
+          {quizs.map((quiz) => (
+            <Quizs key={quiz.id} quiz={quiz}></Quizs>
+          ))}
         </div>
       </section>
-
     </div>
   );
 };
