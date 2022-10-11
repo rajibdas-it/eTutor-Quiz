aec32975ac4e7b25e3f3d2d2892d0d01fba1313d
rajibdas-it <rajibrad@gmail.com>
2022-10-11 16:41:38 +0600

    question ans part done.

--- src/Components/AnswerOptions/AnswerOptions.js
+++ src/Components/AnswerOptions/AnswerOptions.js
@@ -1,21 +1,24 @@
 import { Label, Radio } from 'flowbite-react';
 import React from 'react';
 
-const AnswerOptions = ({ option, qs }) => {
+const AnswerOptions = ({ option, qs, handleQuestionAnswer }) => {
     // console.log(qs);
     const { id, question, correctAnswer, options } = qs;
     return (
-        // border border - gray - 700 rounded - md
-        <div className="flex items-center gap-2">
-            <Radio
-                id={id}
-                name={question}
-                value={option}
-            />
-            <Label htmlFor={id}>
-                {option}
-            </Label>
+        // border border - gray - 700 rounded - md handleQuestionAnswer
+        <div className="flex items-center gap-2" onChange={(e) => handleQuestionAnswer(e.target.value)}>
+            <input type="radio" value={option} name={question} /> {option}
         </div>
+        // <div className="flex items-center gap-2">
+        //     <Radio
+        //         id={id}
+        //         name={question}
+        //         value={option}
+        //     />
+        //     <Label htmlFor={id}>
+        //         {option}
+        //     </Label>
+        // </div>
     );
 };
 
--- src/Components/QuizDetails/QuizDetails.js
+++ src/Components/QuizDetails/QuizDetails.js
@@ -1,17 +1,31 @@
 import { Card } from 'flowbite-react';
-import React from 'react';
+import React, { useState } from 'react';
 import AnswerOptions from '../AnswerOptions/AnswerOptions';
 import { EyeIcon } from '@heroicons/react/24/solid'
-// import { toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 import Swal from 'sweetalert2'
 
 const QuizDetails = ({ qs, index }) => {
     // console.log(qs);
+    const [rightAns, setRightAns] = useState(0)
+    const [wrongAns, setWrongAns] = useState(0)
+
     const { id, question, correctAnswer, options } = qs;
 
     const handleShowAns = (qs) => {
         // toast.info(qs.correctAnswer, { autoClose: 1000 })
         Swal.fire("Correct Answer", qs.correctAnswer)
+    }
+
+    const handleQuestionAnswer = (ans) => {
+        if (ans === correctAnswer) {
+            setRightAns(rightAns + 1)
+            toast.success("Right Answer", { autoClose: 500 })
+
+        } else {
+            setWrongAns(rightAns + 1)
+            toast.error("Wrong Answer", { autoClose: 500 })
+        }
     }
 
     return (
@@ -27,7 +41,7 @@ const QuizDetails = ({ qs, index }) => {
             </div>
             <div className='grid grid-cols-2 gap-4 shadow-lg p-5'>
                 {
-                    options.map((option, index) => <AnswerOptions key={index} option={option} qs={qs}></AnswerOptions>)
+                    options.map((option, index) => <AnswerOptions key={index} option={option} qs={qs} handleQuestionAnswer={handleQuestionAnswer}></AnswerOptions>)
                 }
 
             </div>
