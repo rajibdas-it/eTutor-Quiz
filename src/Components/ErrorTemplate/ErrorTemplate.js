0b51762e4da3b81a1c88075212f3d0e466bc41c0
rajibdas-it <rajibrad@gmail.com>
2022-10-11 21:25:53 +0600

    error page design done

--- src/Components/ErrorTemplate/ErrorTemplate.js
+++ src/Components/ErrorTemplate/ErrorTemplate.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { useRouteError } from "react-router-dom";
+import errorImg from "../../Assets/error.gif";
 
 const ErrorTemplate = () => {
+  const error = useRouteError();
+  console.log(error);
   return (
-    <div>
-      <h1>404 Page Not Found</h1>
+    <div className=" w-[70%] mx-auto">
+      <div className="w-[60%] mx-auto mt-5 text-center">
+        <img src={errorImg} alt="" />
+      </div>
+      <div>
+        {error && (
+          <div>
+            <h1 className="text-center text-5xl font-bold italic mb-5">
+              {error.status}
+            </h1>
+            <p className="text-center text-lg font-bold italic">
+              {error.statusText || error.message}
+            </p>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
