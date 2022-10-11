d43c056b8ff1db8ad8688492192e959d02e1fb1f
rajibdas-it <rajibrad@gmail.com>
2022-10-12 02:01:05 +0600

    statistic page done

--- src/Components/Statistics/Statistics.js
+++ src/Components/Statistics/Statistics.js
@@ -1,9 +1,79 @@
-import React from "react";
+import React, { useContext } from "react";
+import { QuizContext } from "../layout/Main";
+
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  Line,
+  LineChart,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
 const Statistics = () => {
+  const quizs = useContext(QuizContext);
+
   return (
-    <div>
-      <h1>This is statistic page</h1>
+    <div className="w-[90%] mx-auto m-5 mt-10">
+      <h1 className="text-center text-3xl font-bold mb-3 text-blue-400 ">
+        Quiz Informations using Chart
+      </h1>
+      <hr className="w-[70%] mx-auto mb-8" />
+      <div className="grid grid-cols-1 md:grid-cols-2 justify-center gap-4">
+        <div className="flex flex-col-reverse lg:flex-row justify-center items-center">
+          <div className="m-5">
+            <ResponsiveContainer width={320} height={400}>
+              <LineChart
+                width={320}
+                height={400}
+                data={quizs}
+                margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis dataKey="total" />
+                <Tooltip />
+                <Legend />
+
+                <Line type="monotone" dataKey="total" stroke="#82ca9d" />
+              </LineChart>
+            </ResponsiveContainer>
+          </div>
+          <div>
+            <h1 className="text-center font-bold text-blue-400">
+              Line Chart of Quiz
+            </h1>
+            <p className="text-sm text-gray-400 italic text-center">
+              Topic Name and Total Quiz
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col-reverse lg:flex-row justify-center items-center">
+          <div className="text-center m-5">
+            <ResponsiveContainer width={320} height={400}>
+              <BarChart width={350} height={400} data={quizs}>
+                <Bar dataKey="total" fill="#8884d8" />
+                <XAxis dataKey="name" />
+                <YAxis dataKey="total" />
+                <Tooltip />
+              </BarChart>
+            </ResponsiveContainer>
+          </div>
+          <div>
+            <h1 className="text-center font-bold text-blue-400">
+              Bar Chart of Quiz
+            </h1>
+            <p className="text-sm text-gray-400 italic text-center">
+              Topic Name and Total Quiz
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
